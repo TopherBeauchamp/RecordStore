08a90d8a43e6b1fd8774c5c290067ababdbcfd8a
Topher123 <topher.beauchamp@gmail.com>
2024-08-27 23:31:51 -0700

    This is the beginning of the front end, including the HomeScreen, ProductScreen, rating system, product display system, and a footer and header for all screens

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,19 @@
-import React from 'react'
-
+import Header from './components/Header.jsx';
+import Footer from './components/Footer.jsx';
+import { Container } from 'react-bootstrap';
+import { Outlet } from 'react-router-dom';
 const App = () => {
   return (
-    <h1>Welcome to Proshop</h1>
+    <>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <Outlet />
+        </Container> 
+      </main>
+      <Footer />
+    </>
   )
-}
+};
 
-export default App
+export default App;
