7e245801fc25b972d6b9b9cce74bdb22f8bf8a0e
topher <topher.beauchamp@gmail.com>
2024-11-21 22:52:44 -0800

    working payment page + paypal integration/profile page w/ order data complete

--- backend/server.js
+++ backend/server.js
@@ -10,15 +10,13 @@ import {notFound, errorHandler} from './middleware/errorMiddleware.js';
 const port = process.env.PORT; 
 
 connectDB();
-
 const app = express();
-
 
 // Body parser middleware 
 app.use(express.json()); 
 app.use(express.urlencoded({extended: true}));
 
-// Cookie [parser middleware 
+// Cookie [parser middleware]
 app.use(cookieParser());
 
 
@@ -29,6 +27,8 @@ app.get('/', (req, res) => {
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/orders', orderRoutes);
+
+app.get('/api/config/paypal', (req, res) => res.send({clientId: process.env.PAYPAL_CLIENT_ID}));
 
 
 app.use(notFound); 
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -23,6 +23,9 @@ import ShippingScreen from './screens/ShippingScreen.jsx';
 import PrivateRoute from './components/PrivateRoute.jsx';
 import PaymentScreen from './screens/PaymentScreen.jsx';
 import PlaceOrderScreen from './screens/PlaceOrderScreen.jsx';
+import OrderScreen from './screens/OrderScreen.jsx';
+import { PayPalScriptProvider } from '@paypal/react-paypal-js';
+import ProfileScreen from './screens/ProfileScreen.jsx';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -36,6 +39,8 @@ const router = createBrowserRouter(
         <Route path='/shipping' element={<ShippingScreen />} /> 
         <Route path='payment' element={<PaymentScreen />} />
         <Route path='/placeorder' element={<PlaceOrderScreen />} />
+        <Route path='/order/:id' element={<OrderScreen />} /> 
+        <Route path='/profile' element={<ProfileScreen />} />
       </Route>
     </Route>
   )
@@ -45,7 +50,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <RouterProvider router={router} />
+       <PayPalScriptProvider deferLoading={ true }>
+        <RouterProvider router={router} />
+       </PayPalScriptProvider>
     </Provider>
   </React.StrictMode>
 );
