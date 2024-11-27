6db8a31e4b994c9b5986bb9309d481e199df8fe5
topher <topher.beauchamp@gmail.com>
2024-11-26 22:59:21 -0800

    Edit product page working - image uploading process next

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,8 +1,8 @@
 import express from 'express';
 const router = express.Router(); 
-import {getProducts, getProductByID} from '../controllers/productController.js';
-
-router.route('/').get(getProducts);
-router.route('/:id').get(getProductByID);
+import {getProducts, getProductByID, createProduct, updateProduct } from '../controllers/productController.js';
+import { protect, admin } from '../middleware/authMiddleware.js';
+router.route('/').get(getProducts).post(protect, admin, createProduct);
+router.route('/:id').get(getProductByID).put(protect, admin, updateProduct);
 
 export default router;  
